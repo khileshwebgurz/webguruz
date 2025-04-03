357a16220e1dc731d243356329bde2e05de240e1
khileshwebgurz <khilesh@webguruz.in>
2025-04-03 16:15:20 +0530

    pushi

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,26 +1,12 @@
-import axios from "axios";
-import { createHubSpotContact, updateContact } from "../../../../lib/hubspotContact";
-const HUBSPOT_API_KEY = process.env.HUBSPOT_KEY;
 export async function POST(req) {
   try {
     const body = await req.json();
 
-    console.log("🔔 Full PayPal Webhook Data:", JSON.stringify(body, null, 2));
-
     const myEmail = body?.resource?.custom_id || "No email provided";
     const myUrl = body?.resource?.invoice_id || "No URL Provided";
-    const firstName = body?.resource?.seller_protection?.firstName;
-    const lastName = body?.resource?.seller_protection?.lastName;
-    let status = "Unsuccessful";
-
-    const sellerProtection = body?.resource?.seller_protection?.status || "UNKNOWN";
-    const grossAmount = body?.resource?.seller_receivable_breakdown?.gross_amount?.value || "N/A";
-    const paypalFee = body?.resource?.seller_receivable_breakdown?.paypal_fee?.value || "N/A";
-    const netAmount = body?.resource?.seller_receivable_breakdown?.net_amount?.value || "N/A";
-
-
-    const relatedIds = body?.resource?.supplementary_data?.related_ids || [];
-
+    const firstName = body?.resource?.payer?.name?.given_name || "No first name";
+    const lastName = body?.resource?.payer?.name?.surname || "No last name";
+    const status = "Unsuccessful";
 
     if(body?.resource?.status === "COMPLETED") status = "Successful";
 
@@ -29,46 +15,15 @@ export async function POST(req) {
       message: "Webhook received successfully",
       myEmail,
       myUrl,
-      sellerProtection,
-      grossAmount,
-      paypalFee,
-      netAmount,
-      relatedIds
     };
-
+    console.log("my extracted data is >>>", body);
+    console.log('firstname and lastname is >>>>',firstName , lastName)
+    console.log('my some data is >>>',body?.resource?.payer);
     console.log("Extracted Data:", responseData);
 
+    
 
-    const requestBody = {
-      limit: 10,
-      after: null,
-      properties: ["firstname", "lastname", "email", "paypal_payment_status", "website"],
-      filterGroups: [{ filters: [{ propertyName: "email", value: myEmail, operator: "EQ" }] }]
-    };
-
-    const response = await axios.post(
-      "https://api.hubapi.com/crm/v3/objects/contacts/search",
-      requestBody,
-      {
-        headers: {
-          Authorization: `Bearer ${HUBSPOT_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    )
-
-    let message = "";
-    if(response.data.results.length > 0){
-      await updateContact(response.data.results[0].id, HUBSPOT_API_KEY);
-      message= "Contact Updated";
-    }
-    else{
-      await createHubSpotContact(myEmail,firstName, lastName,status,myUrl, HUBSPOT_API_KEY);
-      message = "Contact Created";
-    }
-
-
-    return new Response(JSON.stringify({message: message}), {
+    return new Response(JSON.stringify(responseData), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
